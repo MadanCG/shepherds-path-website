30012e006b252852345b033308c14d1b7bbaf07c
Madan C G <madancgmadan@gmail.com>
2025-07-19 23:51:42 +0530

    Folder structure modified and javascript added to load header in every page

---
+++ Getting_Start/home.js
@@ -0,0 +1,22 @@
+// Load header
+fetch("Home-Header.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("header").innerHTML = data;
+
+    // Add scroll event listener AFTER header is loaded (optional)
+    window.addEventListener("scroll", function () {
+      const header = document.querySelector("header");
+      if (window.scrollY > 500) {
+        header.classList.add("shrink");
+      } else {
+        header.classList.remove("shrink");
+      }
+    });
+  });
+
+fetch("../Footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+  });
